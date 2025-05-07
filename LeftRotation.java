755351bcae7890780ffee1c6cd5d32c360cf46cc
Chirag <chiraggarg1708@gmail.com>
2025-05-07 10:14:50 +0530

    [Chirag] ADD: Solved hackerrank question https://www.hackerrank.com/challenges/array-left-rotation/problem

---
+++ LeftRotation.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class ResultRotation {
+
+    public static List<Integer> rotateLeft(int d, List<Integer> arr) {
+        List<Integer> result = new ArrayList<>();
+
+        for(int i=d;i<arr.size();i++) {
+            result.add(arr.get(i));
+        }
+        for(int i=0;i<d;i++) {
+            result.add(arr.get(i));
+        }
+
+        return result;
+    }
+
+}
+
+public class LeftRotation {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = ResultRotation.rotateLeft(d, arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
