4510a5f476fa12f4d882defa2a7fabe8eaa213f4
Chirag <chiraggarg1708@gmail.com>
2025-05-14 10:18:18 +0530

    [Chirag] ADD: Solved leetcode question https://leetcode.com/problems/first-missing-positive/

---
+++ FirstMissingPositive.java
@@ -0,0 +1,23 @@
+class FirstMissingPositive {
+    public int firstMissingPositive(int[] arr) {
+        int i=0;
+
+        while(i<arr.length) {
+            int index=arr[i]-1;
+            if(arr[i]>0 && arr[i]<arr.length && arr[i]!=arr[index]) {
+                int temp=arr[i];
+                arr[i]=arr[index];
+                arr[index]=temp;
+            } else {
+                i++;
+            }
+        }
+
+        for(i=0;i<arr.length;i++) {
+            if(arr[i]!=i+1) {
+                return i+1;
+            }
+        }
+        return arr.length+1;
+    }
+}
